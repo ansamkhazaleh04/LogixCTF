3818395b1203c17b5f6308de9c03a7964bce7c68
Ansam <ansamwaled266@gmail.com>
2026-04-03 21:24:59 +0300

    Update install message with motivational quote

--- install.sh
+++ install.sh
@@ -1,25 +1,15 @@
 #!/bin/bash
-
 echo "Installing LogixCTF..."
-
-rm -rf temp_LogixCTF
-
 git clone https://github.com/ansamkhazaleh04/LogixCTF.git temp_LogixCTF
 cd temp_LogixCTF || exit
-
 chmod +x main.py
 
 echo
-echo "=============================="
-echo "   LogixCTF Installation Done   "
-echo "=============================="
-echo
-
-echo "Running LogixCTF demo on test.log..."
-python3 main.py test.log
-
+echo "████████████████████████████████████"
+echo "  Learning is the gateway to success"
+echo "████████████████████████████████████"
+echo "     LogixCTF Installation Done"
+echo "████████████████████████████████████"
 echo
 echo "Done! You can now run LogixCTF on any log file with:"
 echo "python3 main.py <your_log_file>"
-
-# temporary change
